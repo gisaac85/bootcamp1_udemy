bea20fc4c71456f710f719165973710deecae6f3
Gaorieh Isaac <gaorieh@hotmail.com>
2018-10-23 12:09:50 +0200

    JS module 11 ex

--- script.js
+++ script.js
@@ -1,35 +1,55 @@
-var count = -10;
-while (count <= 19) {
-    console.log(count);
-    count++;
+// var count = -10;
+// while (count <= 19) {
+//     console.log(count);
+//     count++;
+// }
+
+// console.log('===============================');
+
+// var even = 10;
+// while (even <= 40) {
+//     if (even % 2 === 0) {
+//         console.log(even);
+//     }
+//     even++;
+// }
+
+// console.log('===============================');
+
+// var odd = 300;
+// while (odd <= 333) {
+//     if (odd % 2 !== 0) {
+//         console.log(odd);
+//     }
+//     odd++;
+// }
+
+// console.log('===============================');
+
+// var num = 5;
+// while (num <= 50) {
+//     if ((num % 3 === 0) && (num % 5 === 0)) {
+//         console.log(num);
+//     }
+//     num++;
+// }
+
+for (var i = -10; i <= 19; i++) {
+    console.log(i);
 }
-
 console.log('===============================');
-
-var even = 10;
-while (even <= 40) {
-    if (even % 2 === 0) {
-        console.log(even);
-    }
-    even++;
+for (var i = 10; i <= 40; i += 2) {
+    console.log(i);
 }
-
 console.log('===============================');
-
-var odd = 300;
-while (odd <= 333) {
-    if (odd % 2 !== 0) {
-        console.log(odd);
+for (var i = 300; i <= 333; i++) {
+    if (i % 2 !== 0) {
+        console.log(i);
     }
-    odd++;
 }
-
 console.log('===============================');
-
-var num = 5;
-while (num <= 50) {
-    if ((num % 3 === 0) && (num % 5 === 0)) {
-        console.log(num);
+for (var i = 5; i <= 50; i++) {
+    if (i % 15 === 0) {
+        console.log(i);
     }
-    num++;
 }
